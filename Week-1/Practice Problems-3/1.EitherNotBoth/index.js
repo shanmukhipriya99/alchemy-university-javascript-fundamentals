f5c0732726966950ffa07dbd1f6d6691ee63355f
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2022-12-02 13:30:24 -0500

    eitherNotBoth

---
+++ Week-1/Practice Problems-3/1.EitherNotBoth/index.js
@@ -0,0 +1,7 @@
+function eitherNotBoth(num) {
+    if(num%3===0 && num%5!==0) return true
+    else if(num%5===0 && num%3!==0) return true
+    else return false
+}
+
+module.exports = eitherNotBoth;
