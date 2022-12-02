cb4c3fed155ae1004caf147b96a2ac6b68d6534c
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2022-12-02 09:44:27 -0500

    removeOccurence

---
+++ Week-1/6. Arrays/6.removeOccurences/index.js
@@ -0,0 +1,11 @@
+function removeOccurrences(array, num) {
+    for (let i = array.length - 1; i >= 0; i--) {
+        if(array[i]===num) {
+            array.splice(i, 1)
+        }
+        console.log(array)
+    }    
+    
+}
+
+module.exports = removeOccurrences;
