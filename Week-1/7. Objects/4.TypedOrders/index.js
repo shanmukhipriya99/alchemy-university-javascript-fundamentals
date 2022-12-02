cf8433932244308f39f346d6e6bee6dd3f04bc98
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2022-12-02 10:00:46 -0500

    typedOrders

---
+++ Week-1/7. Objects/4.TypedOrders/index.js
@@ -0,0 +1,13 @@
+const ORDER_TYPES = {
+    PIZZA: 0,
+    WINGS: 1, 
+    SALAD: 2, 
+    BURGER: 3, 
+    FRIES: 4, 
+    SANDWICH: 5,
+    ABC: 6,
+    XYZ: 7
+}
+
+
+module.exports = ORDER_TYPES;
