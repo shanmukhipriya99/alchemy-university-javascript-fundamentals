e12566ef0123e285119b8a9ed22220f055f85c06
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2022-12-04 17:16:30 -0500

    isUnique

---
+++ Week-2/4. Array Reduce/6.IsUnique/index.js
@@ -0,0 +1,14 @@
+// numbers is an array full of integers
+// let's figure if all the numbers are unique
+// i.e. [1,2,3,1] => false
+// [1,2,3] => true
+function allUnique(numbers) {
+    return numbers.reduce((accumulator, currentValue, index) => {
+        if (!accumulator) return false;
+        return numbers.indexOf(currentValue) === index
+        // console.log(accumulator)
+    }, true);
+
+}
+
+module.exports = allUnique;
