32791caee9ed4f1b8868a8af3d9dad343cfb5007
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2022-12-04 22:32:17 -0500

    redoAll

---
+++ Week-2/6. Stacks/6.RedoAll/index.js
@@ -0,0 +1,28 @@
+const Stack = require('./Stack');
+
+class OperationManager {
+    constructor() {
+        this.operations = new Stack();
+        this.undos = new Stack();
+    }
+
+    addOperation(operation) {
+        this.operations.push(operation)
+    }
+
+    undo() {
+        this.undos.push(this.operations.pop())
+    }
+
+    redo() {
+        this.operations.push(this.undos.pop())
+    }
+
+    redoAll() {
+        while(!this.undos.isEmpty()) {
+            this.redo()
+        }
+    }
+}
+
+module.exports = OperationManager;
